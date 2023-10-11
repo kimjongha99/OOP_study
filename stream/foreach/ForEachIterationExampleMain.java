fe31541357da78cf09665732438203e83d7e9cc1
kimjongha99 <jjhsun99@gmail.com>
2023-10-11 17:48:56 +0900

    .

---
+++ stream/foreach/ForEachIterationExampleMain.java
@@ -0,0 +1,28 @@
+package stream.foreach;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForEachIterationExampleMain {
+
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(10);
+        integerList.add(20);
+        integerList.add(30);
+        integerList.add(40);
+        integerList.add(50);
+        integerList.add(60);
+        integerList.add(70);
+        // 익명함수를 내부에서 받는 반복문
+        integerList.stream().forEach(integer -> { //integer변수에 매번 10, 20, 30... 을 번갈아가면서 대입
+            System.out.println(integer);// 해서 콘솔창에 해당 값을 찍어줌
+
+            if(integer == 40){
+                System.out.println("40 찾았다");
+//                throw new RuntimeException("이러면 멈추긴 합니다.");
+                //break 는안먹음, forEach 는 roop 가아님
+            }
+        });
+    }
+    }
